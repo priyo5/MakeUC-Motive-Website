7edf588414410ece5907367688ce3bb47062d87c
Sameer Otham <othams8@gmail.com>
2020-10-03 23:40:19 +0200

    User Object Class

---
+++ User.js
@@ -0,0 +1,46 @@
+class User{
+    constructor(fName, sName, ID, email, password)
+    {
+        this.fName = fName;
+        this.sName = sName;
+        this.ID = ID;
+        this.email = email;
+        this,password = password;
+    }
+    set firstname(inFname){
+        this.fName = inFname;
+    }
+    get firstname()
+    {
+        return this.fName;
+    }
+    set surname(inSname)
+    {
+        this.sName  =inSname;
+    }
+    set IDen(inID)
+    {
+        this.ID = inID;
+    }
+    get ID()
+    {
+        return this.ID;
+    }
+    set mail(inEmail)
+    {
+        this.email = inEmail;
+    }
+    get mail()
+    {
+        return this.email;
+    }
+    set pass (inPass)
+    {
+        this.password = inPass;
+    }
+    get pass()
+    {
+        return this.password;
+    }
+
+}
